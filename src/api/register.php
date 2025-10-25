f56eca71f81f1b2366b2db7561c5d0691a2747fc
FNOR <nawapon_somruang@cmu.ac.th>
2025-10-25 14:47:22 +0700

    feat(admin): add RoleManage page and improve user role creation flow

--- src/api/register.php
+++ src/api/register.php
@@ -1,8 +1,13 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Content-Type: application/json; charset=UTF-8");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+  http_response_code(200);
+  exit();
+}
 
 $servername = "127.0.0.1:3307";
 $username = "root";
--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,7 @@ import Comment from "@/pages/Comment.vue";
 import Login from "@/pages/Login.vue";
 import NewsManage from "@/pages/AdminDashboard/NewsManage.vue";
 import CommentsManage from "@/pages/AdminDashboard/CommentsManage.vue";
+import RoleManage from "@/pages/AdminDashboard/RoleManage.vue";
 
 const routes = [
   { path: "/", name: "login", component: Login },
@@ -23,6 +24,11 @@ const routes = [
     path: "/admin/delete-comment",
     name: "CommentsManage",
     component: CommentsManage,
+  },
+  {
+    path: "/admin/change-role",
+    name: "RoleManage",
+    component: RoleManage,
   },
 ];
 
